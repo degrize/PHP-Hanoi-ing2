febee969d799aa1dbd9f50cc9b516e7b96915c34
luis-borges <kdegrize@gmail.com>
2022-11-18 07:17:15 +0000

    implementation de la  avatar

--- client-web/pages/Panel/vue/avatar/assets/js/main.js
+++ client-web/pages/Panel/vue/avatar/assets/js/main.js
@@ -9,13 +9,25 @@ const showModal = (openButton, modalContent) =>{
         })
     }
 }
-showModal('open-modal','modal-container')
+showModal('open-modal','modal-container');
+showModal('open-modalAvatar','modal-containerAvatar');
 
 /*=============== CLOSE MODAL ===============*/
 const closeBtn = document.querySelectorAll('.close-modal')
+const closeBtnAvatar = document.querySelectorAll('.close-modalAvatar');
+
+function closeModalAvatar(){
+    const modalContainer = document.getElementById('modal-containerAvatar');
+    modalContainer.classList.remove('show-modal');
+    return true;
+}
 
 function closeModal(){
-    const modalContainer = document.getElementById('modal-container')
-    modalContainer.classList.remove('show-modal')
+    const modalContainer = document.getElementById('modal-container');
+    modalContainer.classList.remove('show-modal');
+    return true;
 }
+
+
 closeBtn.forEach(c => c.addEventListener('click', closeModal))
+closeBtnAvatar.forEach(c => c.addEventListener('click', closeModalAvatar))
